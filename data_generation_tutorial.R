66bf6df5c5ccce52344c77ca935ebe36958518cf
marcushjorund <marcushjorund@gmail.com>
2025-01-06 12:18:26 +0100

    Final commit for project on causal inference

--- data_generation_tutorial.R
+++ data_generation_tutorial.R
@@ -1,13 +1,12 @@
-options(scipen=1, digits = 2)
 #true parameter values
-psi <- c(0.0, 0.1, 0.2)
+psi <- c(0, 0.1, 0.2)
 names(psi) <- c("psi21", "psi32", "psi31")
 expit <- function(x){
   1/(1+exp(-x))
 }
 #sample size
 n <- 1e5
-generate_data <- function(n, linear = TRUE){
+generate_data <- function(n, psi, Y3L2coef, linear = TRUE, censoring = FALSE, test = FALSE){
   #generate baseline time-varying and time-invariant confounders
   U <- rnorm(n)
   C <- rnorm(n)
@@ -23,7 +22,7 @@ generate_data <- function(n, linear = TRUE){
   if(linear){
     Y2 <- Y2 + Y1 + L1
   } else{
-    Y2 <- Y2 + 0.1*exp(Y1)
+    Y2 <- Y2 + 0.1*exp(Y1)+0.1*sqrt(abs(L1))
   }
   X2.logit <- -0.1*X1 + 0.2*L2 + 0.2*Y2 + 0.1*L1 + 0.1*Y1 + 0.1*C
   X2 <- rbinom(n,1,expit(X2.logit))
@@ -31,15 +30,160 @@ generate_data <- function(n, linear = TRUE){
   #t = 3
   Y3 <- psi["psi32"]*X2 + psi["psi31"]*X1 + C + U + rnorm(n) 
   if(linear){
-    Y3 <- Y3 + Y2+Y1
+    if(test){
+      Y3 <- Y3 + Y2+ Y1 + L1+Y3L2coef*L2
+    }
+    else{
+      Y3 <- Y3 + Y2 + Y1 
+    }
   } else{
-    Y3 <- Y3 + pnorm(Y2)*exp(Y1)
+    Y3 <- Y3 + pnorm(Y2)*exp(Y1)+Y3L2coef*L2 + L1
   }
-  return(data.frame(C,L1,Y1,X1,L2,Y2,X2,Y3))
+  # Censoring at t=2
+  if(censoring){
+    C2.logit <- -2 + 0.5*X1 + 0.3*L1 + 0.2*Y1 +C + U
+    C2 <- rbinom(n, 1, expit(C2.logit))
+    
+    # Apply censoring at t=2
+    L2[C2 == 1] <- NA
+    Y2[C2 == 1] <- NA
+    X2[C2 == 1] <- NA
+    
+    # t = 3 (only for uncensored individuals)
+    Y3 <- rep(NA, n)
+    uncensored <- which(C2 == 0)
+    Y3[uncensored] <- psi["psi32"]*X2[uncensored] + psi["psi31"]*X1[uncensored] + 
+      C[uncensored] + U[uncensored] + rnorm(length(uncensored))
+    
+    if(linear){
+      if(test){
+        Y3[uncensored] <- Y3[uncensored] + Y2[uncensored] + Y1[uncensored] + 
+          L1[uncensored] + Y3L2coef*L2[uncensored]
+      } else {
+        Y3[uncensored] <- Y3[uncensored] + Y2[uncensored] + Y1[uncensored]
+      }
+    } else {
+      Y3[uncensored] <- Y3[uncensored] + pnorm(Y2[uncensored])*exp(Y1[uncensored]) + Y3L2coef*L2[uncensored] + L1[uncensored]
+    }
+    
+    # Censoring at t=3 (only for those not censored at t=2)
+    C3 <- rep(1, n)
+    C3.logit <--2+ 0.5*X2 + 0.3*L2 + 0.2*Y2+ C + U
+    C3[uncensored] <- rbinom(length(uncensored), 1, expit(C3.logit[uncensored]))
+    
+    # Apply censoring at t=3
+    Y3[C3 == 1] <- NA
+    return(data.frame(C,L1,Y1,X1,L2,Y2,X2,Y3,U,C2,C3))
+  }
+  else{
+    return(data.frame(C,L1,Y1,X1,L2,Y2,X2,Y3,U))
+  }
 }
-set.seed(42)
-generated_data <- generate_data(n)
-head(generated_data)
+set.seed(9000)
+generated_data <- generate_data(n, psi, 0, censoring = TRUE, test = TRUE,linear = FALSE)
+sum(generated_data$C2)/n
+sum(generated_data$C3)
+(sum(generated_data$C3)-sum(generated_data$C2))/sum(1-generated_data$C2)
 write.csv(generated_data, "C:/Users/marcu/OneDrive/Dokumenter/Master/masters-master/generated_data.csv", row.names = FALSE)
 
 
+generate_data_test <- function(n, psi, Y3L2coef, linear = TRUE, censoring = FALSE, test = FALSE) {
+
+  # Unmeasured confounder
+  U <- rnorm(n)
+  
+  # Baseline confounder
+  C_base <- rnorm(n)
+  
+  # Time t = 1
+  L.1 <- C_base + U + rnorm(n)
+  Y_base <- C_base + U + rnorm(n)
+  A1.logit <- 0.35 * L.1 + 0.35 * Y_base + 0.1 * C_base
+  A.1 <- rbinom(n, 1, expit(A1.logit))
+  
+  # Time t = 2
+  L.2 <- -1 * L.1 + A.1 +Y_base+ C_base + U + rnorm(n)
+  Y.2 <- psi["psi21"] * A.1 + C_base + U + rnorm(n)
+  if (linear) {
+    Y.2 <- Y.2 + Y_base + L.1
+  } else {
+    Y.2 <- Y.2 + 0.1 * exp(Y_base) + 0.1 * sqrt(abs(L.1))
+  }
+  A2.logit <- -0.1 * A.1 + 0.2 * L.2 + 0.2 * Y.2 + 0.1 * L.1 + 0.1 * Y_base + 0.1 * C_base
+  A.2 <- rbinom(n, 1, expit(A2.logit))
+  
+  # Time t = 3
+  Y.3 <- psi["psi32"] * A.2 + psi["psi31"] * A.1 + C_base + U + rnorm(n)
+  if (linear) {
+    if (test) {
+      Y.3 <- Y.3 + Y.2 + Y_base + L.1 + Y3L2coef * L.2
+    } else {
+      Y.3 <- Y.3 + Y.2 + Y_base
+    }
+  } else {
+    Y.3 <- Y.3 + pnorm(Y.2) * exp(Y_base) + Y3L2coef * L.2 + L.1
+  }
+  
+  # Censoring mechanism
+  C.2 <- C.3 <- rep(0, n)
+  
+  if (censoring) {
+    # Censoring at t = 2
+    C2.logit <- -2 + 0.5 * A.1 + 0.3 * L.1 + 0.2 * Y_base + C_base + U
+    C.2 <- rbinom(n, 1, expit(C2.logit))
+    
+    L.2[C.2 == 1] <- NA
+    Y.2[C.2 == 1] <- NA
+    A.2[C.2 == 1] <- NA
+    
+    # Censoring at t = 3
+    C3.logit <- -2 + 0.5 * A.2 + 0.3 * L.2 + 0.2 * Y.2 + C_base + U
+    C.3 <- rbinom(n, 1, expit(C3.logit))
+    C.3[C.2 == 1] <- 1
+    
+    Y.3[C.3 == 1] <- NA
+  }
+  
+  # Combine all variables in wide format
+  id <- 1:n
+  data_wide <- as.data.frame(cbind(id, A.1,A.2,L.1,L.2,Y.2,Y.3,C.2,C.3,Y_base,C_base)
+  )
+  print(C.2)
+  print(C.3)
+  # Convert data to long format
+  data_long <- reshape(
+    data_wide,
+    direction = "long",
+    varying = c("A.1", "A.2", "L.1", "L.2", "Y.2", "Y.3", "C.2", "C.3"),
+    v.names = c("A", "L", "Y", "C"),
+    times = c(1, 2),
+    timevar = "time",
+    idvar = "id"
+  )
+
+
+  
+  # Order data by ID and time
+  data_long <- data_long[order(data_long$id, data_long$time), ]
+  data_long$A[seq(1,2*n, by = 2)] <- as.numeric(A.1)
+  data_long$A[seq(2,2*n, by = 2)] <- as.numeric(A.2)
+  data_long$C[seq(1,2*n, by = 2)] <- as.numeric(C.2)
+  data_long$C[seq(2,2*n, by = 2)] <- as.numeric(C.3)
+  # Return the long-format data
+  return(data_long)
+}
+
+n<- 100
+data <- generate_data_test(n, psi, Y3L2coef = 0, censoring = TRUE)
+data
+library(gesttools)
+data <- FormatData(data, idvar = "id",An = "A", varying = c("A", "L", "Y"),timevar = "time", Cn = "C",
+                   GenerateHistory = TRUE, GenerateHistoryMax = 1)
+data$time
+outcomemodels <- list("Y~A+L +Y_base + C_base", "Y~A+L+Lag1Y+Lag1A + Lag1L +Y_base + C_base")
+propensitymodel <- c("A~Lag1A + Y_base + Y+L + Lag1L + C_base")
+censoringmodel <- c("C~Lag1A + Y_base + Y+L + Lag1L + C_base")
+gest <- gestMultiple(data, idvar = "id", timevar = "time", Yn = "Y", An = "A", Cn = "C", 
+                     outcomemodels = outcomemodels, propensitymodel = propensitymodel, 
+                     censoringmodel = censoringmodel, type = 3)
+gest$psi
--- prediction_lm_tutorial.R
+++ prediction_lm_tutorial.R
@@ -1,19 +1,369 @@
-setwd("C:/Users/marcu/OneDrive/Dokumenter/Master/masters-master/")
-generated_data <- read.csv(file = "generated_data.csv", header = TRUE)
+source("data_generation_tutorial.R")
+#Linear outcome model, not propensity score adjusted
+library(ipw)
+library(matrixStats)
+
+psi <- c(1,2,3)
+names(psi) <- c("psi21","psi32","psi31")
+Y3L2coef <-1
+
+#Bootstrapping linear outcome, no propensity score
+B <- 200
+n <- 1e5
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 0
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = TRUE, test = TRUE)
+  #effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2+Y2+X1+L1+L2+Y1+L2+C, data = generated_data)
+  psi32 <- coef(model_y3x2)["X2"]
+  #effect of X1 on Y2
+  model_y2x1 <- lm(Y2~X1+L1+Y1+C, data = generated_data)
+  psi21 <- coef(model_y2x1)["X1"]
+  #residualise 
+  generated_data[,"R32"] <- generated_data[,"Y3"]-psi32*generated_data[,"X2"]
+  model_y3x1 <- lm(R32 ~ L1+X1+Y1+C, na.action = "na.fail",data = generated_data)
+  psi31 <- coef(model_y3x1)["X1"]
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_matrix[b,] <- c(psi_est,coef(model_y3x2)["X1"])
+}
+estimates <- colMeans(psi_matrix)
+biases <- psi-estimates[1:3]
+SD <- sqrt(diag(cov(psi_matrix)))
+CI <- colQuantiles(psi_matrix, probs = c(0.025, 0.975))
+estimates
+biases
+CI
+SD
+
+#Bootstrapping propensity score adjusted linear model
+B <- 200
+n <- 1e5
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 1
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix_propscore_adj <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = TRUE, test = TRUE)
+  p2_model <- glm(X2~X1+Y1+Y2+L1+L2+C, family = binomial("logit"), data = generated_data)
+  p1_model <- glm(X1~Y1 + L1 + C, family = binomial("logit"), data = generated_data)
+  generated_data[,"P2"] <- predict.glm(p2_model, type = "response")
+  generated_data[,"P1"] <- predict.glm(p1_model, type = "response")
+  #effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2+X1+Y2+Y1+L1+L2+C+P2, data = generated_data)
+  psi32 <- coef(model_y3x2)["X2"]
+  
+  #residualise 
+  generated_data[,"R32"] <- generated_data[,"Y3"]-psi32*generated_data[,"X2"]
+  #effect of X1 on Y3
+  model_y3x1 <- lm(R32 ~ X1+Y1+L1+C+P1, data = generated_data)
+  psi31 <- coef(model_y3x1)["X1"]
+  #effect of X1 on Y2
+  
+  model_y2x1 <- lm(Y2~X1+L1+Y1+C+P1, data = generated_data)
+  psi21 <- coef(model_y2x1)["X1"]
+  
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_matrix_propscore_adj[b,] <- c(psi_est, coef(model_y3x2)["X1"])
+}
+
+estimates_propscore_adj <- colMeans(psi_matrix_propscore_adj)
+biases_propscore_adj <- psi-estimates_propscore_adj[1:3]
+SD_propscore_adj <- sqrt(diag(cov(psi_matrix_propscore_adj)))
+CI_propscore_adj <- colQuantiles(psi_matrix_propscore_adj, probs = c(0.025, 0.975))
+#Note that propensity score adjusted increases estimated variances of estimates
+
+#Non linear model, not propensity score adjusted
+B <- 200
+n <- 1e5
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 1
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix_nonlinear <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = FALSE, test = TRUE)
+  #effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2+X1+Y2+Y1+L1+L2+C, data = generated_data)
+  psi32 <- coef(model_y3x2)["X2"]
+  
+  #residualise 
+  generated_data[,"R32"] <- generated_data[,"Y3"]-psi32*generated_data[,"X2"]
+  #effect of X1 on Y3
+  model_y3x1 <- lm(R32 ~ X1+Y1+L1+C, data = generated_data)
+  psi31 <- coef(model_y3x1)["X1"]
+  #effect of X1 on Y2
+  
+  model_y2x1 <- lm(Y2~X1+L1+Y1+C, data = generated_data)
+  psi21 <- coef(model_y2x1)["X1"]
+  
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_matrix_nonlinear[b,] <- c(psi_est, coef(model_y3x2)["X1"])
+}
+
+estimates_nonlinear <- colMeans(psi_matrix_nonlinear)
+biases_nonlinear <- psi-estimates_nonlinear[1:3]
+SD_nonlinear <- sqrt(diag(cov(psi_matrix_nonlinear)))
+CI_nonlinear <- colQuantiles(psi_matrix_nonlinear, probs = c(0.025, 0.975))
+
+#Propensity score adjusted non-linear model
+B <- 200
+n <- 1e5
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 0
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix_nonlinear_propscore_adj <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = FALSE, test = TRUE)
+  p2_model <- glm(X2~X1+Y1+Y2+L1+L2+C, family = binomial("logit"), data = generated_data)
+  p1_model <- glm(X1~Y1 + L1 + C, family = binomial("logit"), data = generated_data)
+  generated_data[,"P2"] <- predict.glm(p2_model, type = "response")
+  generated_data[,"P1"] <- predict.glm(p1_model, type = "response")
+  #effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2+X1+Y2+Y1+L1+L2+C+P2, data = generated_data)
+  psi32 <- coef(model_y3x2)["X2"]
+  
+  #residualise 
+  generated_data[,"R32"] <- generated_data[,"Y3"]-psi32*generated_data[,"X2"]
+  #effect of X1 on Y3
+  model_y3x1 <- lm(R32 ~ X1+Y1+L1+C+P1, data = generated_data)
+  psi31 <- coef(model_y3x1)["X1"]
+  #effect of X1 on Y2
+  
+  model_y2x1 <- lm(Y2~X1+L1+Y1+C+P1, data = generated_data)
+  psi21 <- coef(model_y2x1)["X1"]
+  
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_matrix_nonlinear_propscore_adj[b,] <- c(psi_est, coef(model_y3x2)["X1"])
+}
+estimates_nonlinear_propscore_adj <- colMeans(psi_matrix_nonlinear_propscore_adj)
+biases_nonlinear_propscore_adj <- psi-estimates_nonlinear_propscore_adj[1:3]
+SD_nonlinear_propscore_adj <- sqrt(diag(cov(psi_matrix_nonlinear_propscore_adj)))
+CI_nonlinear_propscore_adj <- colQuantiles(psi_matrix_nonlinear_propscore_adj, probs = c(0.025, 0.975))
+estimates
+estimates_nonlinear
+estimates_propscore_adj
+estimates_nonlinear_propscore_adj
+biases
+biases_nonlinear
+biases_propscore_adj
+biases_nonlinear_propscore_adj
+
+SD
+SD_nonlinear
+SD_propscore_adj
+SD_nonlinear_propscore_adj
+
+CI
+CI_nonlinear
+CI_propscore_adj
+CI_nonlinear_propscore_adj
+
+#Censoring with IPCW estimation
+B <- 200
+n <- 1e4
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 1
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix_censoring <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = FALSE, censoring = TRUE,test = TRUE)
+  #effect of X2 on Y3
+  ipw_c2 <- ipwpoint(exposure = C2, family = "binomial", link = "logit",
+                     denominator = ~ X1 + L1 + Y1+C,
+                     numerator = ~1,
+                     data = generated_data)
+  
+  ipw_c3 <- ipwpoint(exposure = C3, family = "binomial", link = "logit",
+                     denominator = ~ X2 + L2 + Y2 + X1 + L1 + Y1+C,
+                     numerator = ~X1+L1+Y1+C,
+                     data = generated_data[generated_data$C2 == 0, ])
+  # Combine weights
+  generated_data$ipw <- 1
+  generated_data$ipw[generated_data$C2 == 0] <- ipw_c2$ipw.weights[generated_data$C2 == 0]
+  generated_data$ipw[generated_data$C3 == 0] <- ipw_c3$ipw.weights[generated_data[generated_data$C2 ==0,]$C3 == 0]
+  #Propensity scores
+  uncensored_Y2 <- which(!is.na(generated_data$Y2))
+  uncensored_Y3 <- which(!is.na(generated_data$Y3))
+  p2_model <- glm(X2~X1+Y1+Y2+L1+L2+C, family = binomial("logit"),subset = uncensored_Y3,data = generated_data, na.action = na.exclude)
+  p1_model <- glm(X1~Y1 + L1 + C, family = binomial("logit"), subset = uncensored_Y2,data = generated_data, na.action = na.exclude)
+  generated_data[uncensored_Y3,"P2"] <- predict.glm(p2_model, type = "response")
+  generated_data[uncensored_Y2,"P1"] <- predict.glm(p1_model, type = "response")
+  # Effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2 + Y2 + X1 + L1 + L2 + Y1 + C + P2 + P1, 
+                   data = generated_data, 
+                   weights = ipw, 
+                   na.action = na.exclude)
+  psi32 <- coef(model_y3x2)["X2"]
+  
+  # Effect of X1 on Y2
+  model_y2x1 <- lm(Y2 ~ X1 + L1 + Y1 + C + P1, 
+                   data = generated_data, 
+                   weights = ipw, 
+                   na.action = na.exclude)
+  psi21 <- coef(model_y2x1)["X1"]
+  
+  # Residualize
+  generated_data$R32 <- generated_data$Y3 - psi32 * generated_data$X2
+  
+  # Final model
+  model_y3x1 <- lm(R32 ~ X1 + L1 + Y1 + C + P1, 
+                   data = generated_data, 
+                   weights = ipw, 
+                   na.action = na.exclude)
+  psi31 <- coef(model_y3x1)["X1"]
+  
+  # Combine results
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_est
+  psi_matrix_censoring[b,] <- c(psi_est, coef(model_y3x2)["X1"])
+}
+estimates_censoring <- colMeans(psi_matrix_censoring)
+bias_censoring <- psi - estimates_censoring[1:3]
+SD_censoring <- sqrt(diag(cov(psi_matrix_censoring)))
+CI_censoring <- colQuantiles(psi_matrix_censoring, probs = c(0.025, 0.975))
+estimates_censoring
+bias_censoring
+SD_censoring
+CI_censoring
+
+#Censoring naive implementation without IPCW estimation
+B <- 200
+n <- 1e4
+names(psi) <- c("psi21", "psi32", "psi31")
+Y3L2coef <- 1
+#3 columns for the three effect estimates, and one column for the presumed biased estimate for psi31
+psi_matrix_censoring_naive <- matrix(0, nrow = B, ncol = 4)
+for (b in 1:B){
+  generated_data <- generate_data(n, psi, Y3L2coef, linear = TRUE, censoring = TRUE,test = TRUE)
+  #Propensity scores
+  uncensored_Y2 <- which(!is.na(generated_data$Y2))
+  uncensored_Y3 <- which(!is.na(generated_data$Y3))
+  p2_model <- glm(X2~X1+Y1+Y2+L1+L2+C, family = binomial("logit"),subset = uncensored_Y3,data = generated_data, na.action = na.exclude)
+  p1_model <- glm(X1~Y1 + L1 + C, family = binomial("logit"), subset = uncensored_Y2,data = generated_data, na.action = na.exclude)
+  generated_data[uncensored_Y3,"P2"] <- predict.glm(p2_model, type = "response")
+  generated_data[uncensored_Y2,"P1"] <- predict.glm(p1_model, type = "response")
+  # Effect of X2 on Y3
+  model_y3x2 <- lm(Y3 ~ X2 + Y2 + X1 + L1 + L2 + Y1 + C + P2 + P1, 
+                   data = generated_data, 
+                   na.action = na.exclude)
+  psi32 <- coef(model_y3x2)["X2"]
+  
+  # Effect of X1 on Y2
+  model_y2x1 <- lm(Y2 ~ X1 + L1 + Y1 + C + P1, 
+                   data = generated_data, 
+                   na.action = na.exclude)
+  psi21 <- coef(model_y2x1)["X1"]
+  
+  # Residualize
+  generated_data$R32 <- generated_data$Y3 - psi32 * generated_data$X2
+  
+  # Final model
+  model_y3x1 <- lm(R32 ~ X1 + L1 + Y1 + C + P1, 
+                   data = generated_data,
+                   na.action = na.exclude)
+  psi31 <- coef(model_y3x1)["X1"]
+  
+  # Combine results
+  psi_est <- c(psi21, psi32, psi31)
+  names(psi_est) <- c("psi21", "psi32", "psi31")
+  psi_est
+  psi_matrix_censoring_naive[b,] <- c(psi_est, coef(model_y3x2)["X1"])
+}
+estimates_censoring_naive <- colMeans(psi_matrix_censoring_naive)
+bias_censoring_naive <- psi - estimates_censoring_naive[1:3]
+SD_censoring_naive <- sqrt(diag(cov(psi_matrix_censoring_naive)))
+CI_censoring_naive <- colQuantiles(psi_matrix_censoring_naive, probs = c(0.025, 0.975))
+
+library(GGally)
+library(reshape2)
+library(tidyr)
+library(dplyr)
+psi_dataframe_censoring <- as.data.frame(psi_matrix_censoring[,1:3])
+psi_dataframe_censoring_naive <- as.data.frame(psi_matrix_censoring_naive[,1:3])
+
+colnames(psi_dataframe_censoring) <- c("psi21", "psi31", "psi32")
+colnames(psi_dataframe_censoring_naive) <- c("psi21", "psi31", "psi32")
+psi_dataframe_censoring <- psi_dataframe_censoring %>%
+  mutate(matrix = "IPCW censoring estimation")
+psi_dataframe_censoring_naive <- psi_dataframe_censoring_naive %>%
+  mutate(matrix = "Naive censoring estimation")
+combined_df <- bind_rows(psi_dataframe_censoring, psi_dataframe_censoring_naive)
+long_df <- combined_df %>% 
+  pivot_longer(cols = c("psi21", "psi32", "psi31"),
+               names_to = "Parameter",
+               values_to = "Estimate")
+
+ggplot(long_df, aes(x = Parameter, y = Estimate, fill = matrix)) +
+  geom_boxplot() +
+  scale_fill_manual(values = c("IPCW censoring estimation" = "blue", "Naive censoring estimation" = "red")) +
+  theme_minimal() +
+  labs(title = "Comparison of Bootstrapped Estimates",
+       x = "Parameter",
+       y = "Estimate",
+       fill = "Censoring adjustment")
+estimates_censoring
+estimates_censoring_naive
+SD_censoring
+SD_censoring_naive
+CI_censoring
+CI_censoring_naive
+
+#Testing implementation
+n <- 1e5
+generated_data <- generate_data(n, psi, Y3L2coef, linear = FALSE, censoring = TRUE,test = TRUE)
 #effect of X2 on Y3
-model_y3x2 <- lm(Y3 ~ X2+X1+Y2+Y1+L1+L2+C, data = generated_data)
+ipw_c2 <- ipwpoint(exposure = C2, family = "binomial", link = "logit",
+                   denominator = ~ X1 + L1 + Y1+C,
+                   numerator = ~1,
+                   data = generated_data)
+
+ipw_c3 <- ipwpoint(exposure = C3, family = "binomial", link = "logit",
+                   denominator = ~ X2 + L2 + Y2 + X1 + L1 + Y1+C,
+                   numerator = ~X1+L1+Y1,
+                   data = generated_data[generated_data$C2 == 0, ])
+# Combine weights
+generated_data$ipw <- 1
+generated_data$ipw[generated_data$C2 == 0] <- ipw_c2$ipw.weights[generated_data$C2 == 0]
+generated_data$ipw[generated_data$C3 == 0] <- ipw_c3$ipw.weights[generated_data[generated_data$C2 ==0,]$C3 == 0]
+#Propensity scores
+uncensored_Y2 <- which(!is.na(generated_data$Y2))
+uncensored_Y3 <- which(!is.na(generated_data$Y3))
+p2_model <- glm(X2~X1+Y1+Y2+L1+L2+C, family = binomial("logit"),subset = uncensored_Y3,data = generated_data, na.action = na.exclude)
+p1_model <- glm(X1~Y1 + L1 + C, family = binomial("logit"), subset = uncensored_Y2,data = generated_data, na.action = na.exclude)
+generated_data[uncensored_Y3,"P2"] <- predict.glm(p2_model, type = "response")
+generated_data[uncensored_Y2,"P1"] <- predict.glm(p1_model, type = "response")
+# Effect of X2 on Y3
+model_y3x2 <- lm(Y3 ~ X2 + Y2 + X1 + L1 + L2 + Y1 + C + P2 + P1, 
+                 data = generated_data, 
+                 weights = ipw, 
+                 na.action = na.exclude)
 psi32 <- coef(model_y3x2)["X2"]
-#residualise 
-generated_data[,"R32"] <- generated_data[,"Y3"]-psi32*generated_data[,"X2"]
-#effect of X1 on Y3
-model_y3x1 <- lm(R32 ~ C+Y1+L1+X1, data = generated_data)
-psi31 <- coef(model_y3x1)["X1"]
 
-#effect of X1 on Y2
-
-model_y2x1 <- lm(Y2~X1+L1+Y1+C, data = generated_data)
+# Effect of X1 on Y2
+model_y2x1 <- lm(Y2 ~ X1 + L1 + Y1 + C + P1, 
+                 data = generated_data, 
+                 weights = ipw, 
+                 na.action = na.exclude)
 psi21 <- coef(model_y2x1)["X1"]
 
-psi <- c(psi21, psi32, psi31)
-names(psi) <- c("psi21", "psi32", "psi31")
-psi
+# Residualize
+generated_data$R32 <- generated_data$Y3 - psi32 * generated_data$X2
+
+# Final model
+model_y3x1 <- lm(R32 ~ X1 + L1 + Y1 + C + P1, 
+                 data = generated_data, 
+                 weights = ipw, 
+                 na.action = na.exclude)
+psi31 <- coef(model_y3x1)["X1"]
+
+# Combine results
+psi_est <- c(psi21, psi32, psi31)
+names(psi_est) <- c("psi21", "psi32", "psi31")
+psi_est
